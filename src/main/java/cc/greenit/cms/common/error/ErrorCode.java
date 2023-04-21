ffdff219e1f5a81ce1587ebec07f2ac6a3fed4eb
soonhyeong <sun891103@gmail.com>
2023-04-21 18:34:24 +0900

    에러코드 추가

--- src/main/java/cc/greenit/cms/common/error/ErrorCode.java
+++ src/main/java/cc/greenit/cms/common/error/ErrorCode.java
@@ -51,6 +51,7 @@ public enum ErrorCode {
     CHECKIN_NOT_FOUND(400, "V004", "체크인 정보가 없습니다."),
     ALREADY_CHECKIN(400, "V005", "이미 체크인 된 고객입니다."),
     CHECKIN_ONLY_TODAY(400, "V006", "체크인은 당일만 가능합니다."),
+    QRCODE_NOT_FOUND(400, "V007", "없는 qrcode 입니다."),
     ;
 
     private int status;
