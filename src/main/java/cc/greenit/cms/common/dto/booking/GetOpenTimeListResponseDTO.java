6f510a7cf2266a8b665a80eda2d6292ff90d8760
soonhyeong <sun891103@gmail.com>
2023-05-30 15:16:47 +0900

    no message

--- src/main/java/cc/greenit/cms/common/dto/booking/GetOpenTimeListResponseDTO.java
+++ src/main/java/cc/greenit/cms/common/dto/booking/GetOpenTimeListResponseDTO.java
@@ -15,5 +15,11 @@ public class GetOpenTimeListResponseDTO {
     private String dateId;
     private String timeId;
     private String courseId;
+    /**
+     * 모든 에이전시, 홈페이지 오픈이 같으면 ALL
+     * - GOLAGA   : 골라가
+     * - HOMEPAGE : 홈페이지
+     */
     private String agencyCode;
+
 }
