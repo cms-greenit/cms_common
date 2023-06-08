9ea4cb3cf318841140c2e2d08312fd564870717b
soonhyeong <sun891103@gmail.com>
2023-06-08 10:24:39 +0900

    no message

--- src/main/java/cc/greenit/cms/common/dto/booking/GetTimeListResponseDTO.java
+++ src/main/java/cc/greenit/cms/common/dto/booking/GetTimeListResponseDTO.java
@@ -21,6 +21,7 @@ public class GetTimeListResponseDTO {
     // CADDY("캐디동반"), SELF("셀프라운드"), DRIVING("드라이빙캐디"), MARSHALL("마샬캐디"), INTERN("인턴캐디"), SMILE("미소지기캐디"), SPROUT("새싹캐디");
     private String caddyType;
     private String greenFee;
+    private String cartFee;
     private String erpRsvName;
     private String erpRsvNo;
     private String erpRsvPhone;
