1ee6b0661e6b2d869183df7a96e999479e5aee03
soonhyeong <sun891103@gmail.com>
2023-07-10 14:40:10 +0900

    [ADD] 예약 오픈조회 타임 시퀀스 추가

--- src/main/java/cc/greenit/cms/common/dto/booking/GetOpenTimeListResponseDTO.java
+++ src/main/java/cc/greenit/cms/common/dto/booking/GetOpenTimeListResponseDTO.java
@@ -13,8 +13,9 @@ import lombok.Data;
 public class GetOpenTimeListResponseDTO {
     private String companyId;
     private String dateId;
-    private String timeId;
     private String courseId;
+    private String timeId;
+    private String timeSeq;
     /**
      * 모든 에이전시, 홈페이지 오픈이 같으면 ALL
      * - GOLAGA   : 골라가
