3006630a4fb356b8698fcd55f135105712df5b20
soonhyeong <sun891103@gmail.com>
2023-07-13 14:56:43 +0900

    [ADD] 체크인 프로모션 추가

---
+++ src/main/java/cc/greenit/cms/common/dto/visit/PostCheckInPromotionDTO.java
@@ -0,0 +1,20 @@
+package cc.greenit.cms.common.dto.visit;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.visit
+ * fileName       : PostCheckInPromotionDTO
+ * author         : soonhyeong
+ * date           : 2023/07/13
+ * description    :
+ */
+@Data
+public class PostCheckInPromotionDTO {
+    @NotEmpty
+    private String companyId;
+    @NotEmpty
+    private String checkinId;
+}
