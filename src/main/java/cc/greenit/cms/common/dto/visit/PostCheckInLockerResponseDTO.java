e498d66940e853eab480e0a35d5d76618ba6d881
soonhyeong <sun891103@gmail.com>
2023-08-09 13:57:05 +0900

    [ADD] 락커발급 응답 추가

---
+++ src/main/java/cc/greenit/cms/common/dto/visit/PostCheckInLockerResponseDTO.java
@@ -0,0 +1,17 @@
+package cc.greenit.cms.common.dto.visit;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.visit
+ * fileName       : PostCheckInLockerRequestDTO
+ * author         : zaid
+ * date           : 2023/08/09
+ * description    :
+ */
+@Data
+public class PostCheckInLockerResponseDTO {
+    private String locker;
+}
