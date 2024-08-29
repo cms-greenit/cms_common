b52bee74ecf1b9c2fa7ac07217d6f0959cf43e70
will <jphong@greenit.cc>
2024-08-29 14:07:28 +0900

    [ADD] PatchReservationRequestDTO 추가

---
+++ src/main/java/cc/greenit/cms/common/dto/booking/PatchReservationRequestDTO.java
@@ -0,0 +1,39 @@
+package cc.greenit.cms.common.dto.booking;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.booking
+ * fileName       : PatchReservationRequestDTO
+ * author         : jinpyo
+ * date           : 8/29/24
+ * description    :
+ */
+@Data
+public class PatchReservationRequestDTO {
+    @NotEmpty
+    private String companyId;
+    @NotEmpty
+    private String dateId;
+    @NotEmpty
+    private String courseId;
+    @NotEmpty
+    private String timeId;
+    @NotEmpty
+    private String patchDateId;
+    @NotEmpty
+    private String patchCourseId;
+    @NotEmpty
+    private String patchTimeId;
+    private String patchTimeSeq;
+    @NotEmpty
+    private String agencyCode;
+    @NotNull
+    private Integer patchPerson;
+    @Size(max = 50)
+    private String remark;
+}
---
+++ src/main/java/cc/greenit/cms/common/dto/booking/PatchReservationResponseDTO.java
@@ -0,0 +1,17 @@
+package cc.greenit.cms.common.dto.booking;
+
+import lombok.Data;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.booking
+ * fileName       : PatchReservationResponseDTO
+ * author         : jinpyo
+ * date           : 8/29/24
+ * description    :
+ */
+@Data
+public class PatchReservationResponseDTO {
+    private String rsvNo;
+    private String memberId;
+    private String memberSeq;
+}
