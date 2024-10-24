5ef9598cb99fb106b3f0d55aefd16e4b272a1a7f
will <jphong@greenit.cc>
2024-10-24 11:57:25 +0900

    [ADD] 예약 비고 조회/ 비고등록 DTO추가

---
+++ src/main/java/cc/greenit/cms/common/dto/booking/GetBookingRemarkResponseDTO.java
@@ -0,0 +1,12 @@
+package cc.greenit.cms.common.dto.booking;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.booking
+ * fileName       : GetBookingRemarkResponseDTO
+ * author         : jinpyo
+ * date           : 10/24/24
+ * description    :
+ */
+public class GetBookingRemarkResponseDTO {
+    private String remark;
+}
