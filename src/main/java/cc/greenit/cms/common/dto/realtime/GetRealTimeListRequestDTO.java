3dca7c90c513d22df6b410f76b69c1761901ee93
ParkSangun <andrea122245@gmail.com>
2024-11-06 15:26:12 +0900

    [ADD] - Realtime Request

---
+++ src/main/java/cc/greenit/cms/common/dto/realtime/GetRealTimeListRequestDTO.java
@@ -0,0 +1,14 @@
+package cc.greenit.cms.common.dto.realtime;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.realtime
+ * fileName       : GetRealTimeListRequestDTO
+ * author         : zaid
+ * date           : 2024. 11. 6.
+ * description    :
+ */
+public class GetRealTimeListRequestDTO {
+    private String companyId;
+    private String from;
+    private String to;
+}
