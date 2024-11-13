74feeb0f4c9b8f6231451f343a426d78c0003974
ParkSangun <andrea122245@gmail.com>
2024-11-13 16:29:26 +0900

    [ADD] - RealTime Response ADD

---
+++ src/main/java/cc/greenit/cms/common/dto/realtime/GetRealTimeListResponseDTO.java
@@ -0,0 +1,26 @@
+package cc.greenit.cms.common.dto.realtime;
+
+import lombok.Data;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.realtime
+ * fileName       : GetRealTimeListResponseDTO
+ * author         : zaid
+ * date           : 2024. 11. 13.
+ * description    :
+ */
+@Data
+public class GetRealTimeListResponseDTO {
+    private String companyId;
+    private String dateId;
+    private String timeSeq;
+    private String timeId;
+    private String courseId;
+    private String openType;
+    private String agencyOpenYn;
+    private String openAgencyCode;
+    private Integer part;
+    private Integer headcount;
+    private String caddyType;
+    private Integer greenFee;
+}
