ac11efd10d801033725fa009061b36ae19945e72
will <jphong@greenit.cc>
2024-12-20 14:40:03 +0900

    [FIX] 로컬 build 수정

--- src/test/java/cc/greenit/cms/cms_common/CmsCommonApplicationTests.java
+++ src/test/java/cc/greenit/cms/cms_common/CmsCommonApplicationTests.java
@@ -1,9 +1,12 @@
 package cc.greenit.cms.cms_common;
 
+import cc.greenit.cms.common.Main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = Main.class)
 class CmsCommonApplicationTests {
 
 	@Test
