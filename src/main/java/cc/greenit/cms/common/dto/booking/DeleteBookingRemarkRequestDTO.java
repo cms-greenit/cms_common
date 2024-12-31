13fba7b2b0f0277d030589e38fb61d1edd5a379d
will <jphong@greenit.cc>
2024-12-31 09:25:14 +0900

    [ADD] DeleteBookingRemarkRequestDTO

---
+++ src/main/java/cc/greenit/cms/common/dto/booking/DeleteBookingRemarkRequestDTO.java
@@ -0,0 +1,28 @@
+package cc.greenit.cms.common.dto.booking;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.booking
+ * fileName       : PostBookingRemarkRequestDTO
+ * author         : jinpyo
+ * date           : 10/24/24
+ * description    :
+ */
+@Data
+@Builder
+public class DeleteBookingRemarkRequestDTO {
+    @NotEmpty
+    private String companyId;
+    @NotEmpty
+    private String dateId;
+    @NotEmpty
+    private String timeId;
+    @NotEmpty
+    private String courseId;
+    @NotEmpty
+    private String agencyCode;
+}
