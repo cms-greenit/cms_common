14c01d2215a33f3972d0c90ab9803143a8ad7621
will <jphong@greenit.cc>
2025-06-13 15:46:30 +0900

    [ADD] 예약자 정보 수정 DTO 추가

---
+++ src/main/java/cc/greenit/cms/common/dto/booking/PatchBookingBookerInfoRequestDTO.java
@@ -0,0 +1,33 @@
+package cc.greenit.cms.common.dto.booking;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.booking
+ * fileName       : PatchBookingBookerInfoRequestDTO
+ * author         : jinpyo
+ * date           : 6/13/25
+ * description    :
+ */
+@Data
+@Builder
+public class PatchBookingBookerInfoRequestDTO {
+    @NotEmpty
+    private String companyId;
+    @NotEmpty
+    private String dateId;
+    @NotEmpty
+    private String timeId;
+    @NotEmpty
+    private String courseId;
+    @NotEmpty
+    private String memberName;
+    @NotEmpty
+    private String memberPhone;
+    @NotEmpty
+    private String agencyCode;
+
+}
