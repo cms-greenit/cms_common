8432b0fe4c9e47bee258824d25c851e0b92c254a
ParkSangun <andrea122245@gmail.com>
2025-07-14 16:05:35 +0900

    [ADD] - getHomepageCrawler

---
+++ src/main/java/cc/greenit/cms/common/dto/booking/GetHomepageCrawlerVo.java
@@ -0,0 +1,19 @@
+package cc.greenit.cms.common.dto.booking;
+
+import lombok.Data;
+
+/**
+ * packageName    : cc.greenit.cms.common.dto.booking
+ * fileName       : GetHomepageCrawlerVo
+ * author         : zaid
+ * date           : 2025. 7. 14.
+ * description    :
+ */
+@Data
+public class GetHomepageCrawlerVo {
+    private String companyId;
+    private String dateId;
+    private String courseId;
+    private String timeId;
+    private Integer greenFee;
+}
